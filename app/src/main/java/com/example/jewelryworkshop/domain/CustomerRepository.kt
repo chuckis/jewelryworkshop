3e8c32ee72e471eecf02c5e5f17a045effe156bb
chuckis <chuckis94@gmail.com>
2025-06-24 16:46:23 +0300

    move files

---
+++ app/src/main/java/com/example/jewelryworkshop/domain/CustomerRepository.kt
@@ -0,0 +1,12 @@
+package com.example.jewelryworkshop.domain
+
+import kotlinx.coroutines.flow.Flow
+import java.time.LocalDateTime
+
+interface CustomerRepository {
+    suspend fun addCustomer(customer: Customer): Long
+    suspend fun updateCustomer(customer: Customer)
+    suspend fun deleteCustomer(customer: Customer)
+    suspend fun getCustomer(customer: Customer): Customer
+    suspend fun allCustomers(): Flow<List<Customer>>
+}
